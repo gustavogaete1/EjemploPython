ac663a439d16314a028d0bbbeb75a7f454751631
gustavogaete1 <g.gaete.ramos@gmail.com>
2025-06-09 16:27:22 -0400

    calculadora con resta

--- calculadora.py
+++ calculadora.py
@@ -2,5 +2,8 @@ print("CALCULADORA POKEMON")
 
 v1 = int(input("VALOR 1 "))
 v2 = int(input("VALOR 2 "))
-print("EL RESULTADO ES ",v1+v2)
+print("EL RESULTADO ES ",v1+v2")
 
+v1 = int(input("VALOR 1 "))
+v2 = int(input("VALOR 2 "))
+print("EL RESULTADO ES ",v1-v2")
